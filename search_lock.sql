3f7387ba34cb30375714a0a4616461ed9403f9e7
ksiz <kenke.siz@gmail.com>
2013-12-20 13:24:13 +0900

    ロック状況一覧を確認するSQLを追加した

--- search_lock.sql
+++ search_lock.sql
@@ -1,3 +1,42 @@
+-- ロック状況を把握
+select
+    V$SESSION.SID      as SID
+   ,V$SESSION.USERNAME as USERNAME
+   ,V$SESSION.OSUSER   as OSUSER
+   ,V$SESSION.SERIAL#  as SERIAL_NO
+   ,V$SESSION.MACHINE  as MACHINE
+   ,V$SESSION.PROGRAM  as PROGRAM
+   ,V$LOCK.TYPE        as LOCK_TYPE
+   ,V$LOCK.ID1         as ID1
+   ,V$LOCK.ID2         as ID2
+   ,TO_CHAR(V$LOCK.CTIME / 60,'999990.9') as LOCK_TIME
+   ,case V$LOCK.LMODE
+        when 0 then 'NONE'
+        when 1 then 'NULL'
+        when 2 then '  RS'
+        when 3 then '  RX'
+        when 4 then '   S'
+        when 5 then ' SRX'
+        when 6 then '   X'
+               else '   ?' end as HELD
+   ,case V$LOCK.REQUEST
+        when 0 then 'NONE'
+        when 1 then 'NULL'
+        when 2 then '  RS'
+        when 3 then '  RX'
+        when 4 then '   S'
+        when 5 then ' SRX'
+        when 6 then '   X'
+               else '   ?' end as REQUESTED
+from V$SESSION
+    inner join V$LOCK on V$SESSION.SID = V$LOCK.SID
+where
+    V$SESSION.USERNAME like upper('%%')
+order by
+    V$SESSION.SID
+   ,V$LOCK.TYPE
+;
+
 -- ロックをかけているセッションID、ユーザー名、プログラム名、ロックしている時間を取得
 select
     V$SESSION.SID      as SID
